449a04d7c8cc494676379afcc436ed800924caac
chcken05 <michaeladrian.severino@gmail.com>
2024-03-17 12:37:28 +0800

    grid done

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Test!");
+const rows = 16;
+const cols = 16;
+const container = document.querySelector(".container");
+
+for (let i = 0; i < rows; i++) {
+  let gridRow = document.createElement("div");
+  container.appendChild(gridRow).className = "gridRow";
+  for (let j = 0; j < cols; j++) {
+    let cells = document.createElement("div");
+    gridRow.appendChild(cells).className = "cells";
+  }
+}
