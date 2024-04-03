e6e70facf042409d67fd6690d064b3e9cb212c1e
chcken05 <michaeladrian.severino@gmail.com>
2024-04-03 20:51:31 +0800

    update

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-const rows = 16;
-const cols = 16;
+let size = 16;
+const btn = document.querySelector(".btn");
 const container = document.querySelector(".container");
 
-for (let i = 0; i < rows; i++) {
+for (let i = 0; i < size; i++) {
   let gridRow = document.createElement("div");
   container.appendChild(gridRow).className = "gridRow";
-  for (let j = 0; j < cols; j++) {
-    let cells = document.createElement("div");
-    gridRow.appendChild(cells).className = "cells";
+  for (let j = 0; j < size; j++) {
+    let cell = document.createElement("div");
+    gridRow.appendChild(cell).className = "cells";
   }
 }
+
+let cells = document.querySelectorAll(".cells");
+let rows = document.querySelectorAll(".gridRow");
+
+cells.forEach((div) =>
+  div.addEventListener("mouseover", () => {
+    div.classList.add("bg-color");
+  })
+);
+
+function deleteGrid() {
+  cells.forEach((el) => el.classList.remove("cells"));
+  rows.forEach((el) => el.classList.remove(".gridRow"));
+}
+
+function createGrid(size) {
+  deleteGrid();
+
+  for (let i = 0; i < size; i++) {
+    let gridRow = document.createElement("div");
+    container.appendChild(gridRow).className = "gridRow";
+    for (let j = 0; j < size; j++) {
+      let cell = document.createElement("div");
+      gridRow.appendChild(cell).className = "cells";
+    }
+  }
+
+  let cells = document.querySelectorAll(".cells");
+
+  cells.forEach((div) =>
+    div.addEventListener("mouseover", () => {
+      div.classList.add("bg-color");
+    })
+  );
+}
+
+btn.addEventListener("click", () => {
+  size = prompt("Enter the grid size you want");
+  createGrid(size);
+});
